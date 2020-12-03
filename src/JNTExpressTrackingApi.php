63e667e7915a1331b10fe0ac66e6867ae5d52b2e
Akif Rabbani <akif@bayam.my>
2020-12-03 22:48:31 +0800

    Added status

--- src/JNTExpressTrackingApi.php
+++ src/JNTExpressTrackingApi.php
@@ -38,6 +38,7 @@ class JNTExpressTrackingApi
 
 	    if($trackDetails->length > 0) # check if there is records found or not
 		{
+			$trackres['status'] = 1;
 	        $trackres['message'] = "Record Found"; # return record found if number of row > 0
 
 	        // ----- Get Pickup date for Year ----
@@ -86,6 +87,7 @@ class JNTExpressTrackingApi
 	    } 
 	    else 
 	    {
+	    	$trackres['status'] = 0;
 	        $trackres['message'] = "No Record Found"; # return record not found if number of row < 0
 	        # since no record found, no need to parse the html furthermore
 	    }
